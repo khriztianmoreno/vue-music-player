5b7f7ce3ec4814998ea1da7de564728e9473c543
khriztianmoreno <khriztianmoreno@gmail.com>
2018-08-03 09:29:17 -0500

    Tweaking the Template

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
-
+import Player from './Player.vue'
+ 
+import {Howl, Howler} from 'howler'
+ 
 Vue.use(Vuetify)
-
+ 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(Player)
 })
